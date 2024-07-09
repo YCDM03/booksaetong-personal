fcf136251f71ffac5952eb7b80085183e0fe9fb7
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-07-09 16:51:40 +0900

    Style: public img 사용해봄

--- src/app/(provider)/(root)/mypage/layout.tsx
+++ src/app/(provider)/(root)/mypage/layout.tsx
@@ -1,12 +1,14 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 
-function layout({ children }: PropsWithChildren) {
+function MypageLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-row max-w-[1224px] min-h-fit mx-auto my-10">
       <nav className="border w-[200px] h-[400px] py-10 rounded-md">
         <ul className="flex flex-col text-sm cursor-pointer">
           <Link href={'./profile'}>
+            <Image src={'/assets/img/sidebar-img.png'} width={20} height={20} alt="프로필 아이콘" />
             <li className="h-12 hover:bg-hover px-4">프로필 수정</li>
           </Link>
           <Link href={'./likes'}>
@@ -22,4 +24,4 @@ function layout({ children }: PropsWithChildren) {
   );
 }
 
-export default layout;
+export default MypageLayout;
