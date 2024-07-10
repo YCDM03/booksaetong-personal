9cf0ecf981b675929a951c7144ec09bd85382de5
yeeunseo-dev <anfrhrl96@gmail.com>
2024-07-10 20:26:20 +0900

    feat:지도위치불러오기

--- src/app/(provider)/(root)/(product)/detail/[id]/page.tsx
+++ src/app/(provider)/(root)/(product)/detail/[id]/page.tsx
@@ -5,12 +5,14 @@ import Location from '@/components/Detail/Location';
 import ProductCard, { Product } from '@/components/Detail/ProductCard';
 import ProductIntro from '@/components/Detail/ProductIntro';
 import { supabase } from '@/contexts/supabase.context';
+import Script from 'next/script';
 import { useEffect, useState } from 'react';
 
 function DetailPage({ params }: { params: { id: string } }) {
   const [products, setProducts] = useState<Product[]>([]);
   const [productImages, setProductImages] = useState<{ [key: string]: string[] }>({});
   const [userData, setUserData] = useState<any[]>([]);
+  const [isKakaoMapLoaded, setIsKakaoMapLoaded] = useState(false);
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
@@ -63,12 +65,29 @@ function DetailPage({ params }: { params: { id: string } }) {
     fetchData();
   }, [params.id]);
 
+  useEffect(() => {
+    // Check if Kakao SDK script is already loaded
+    const isKakaoSDKLoaded = !!document.querySelector('script[src*="sdk.js"]');
+    setIsKakaoMapLoaded(isKakaoSDKLoaded);
+  }, []);
+
   return (
     <div className="flex justify-center">
       <div className="container mx-auto w-11/12 lg:w-[1440px] flex flex-col items-center">
         <ProductCard products={products} productImages={productImages} userData={userData} />
         <ProductIntro productId={params.id} />
-        <Location />
+        {products.length > 0 && (
+          <Location
+            latitude={products[0]?.latitude || 0}
+            longitude={products[0]?.longitude || 0}
+            address={products[0]?.address || ''}
+          />
+        )}
+        {isKakaoMapLoaded && (
+          <Script
+            src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`}
+          />
+        )}
         <div className="w-full lg:w-[1000px] py-8">
           <h6 className="text-2xl font-bold mb-2">최근 도서</h6>
           <div>카드</div>
--- src/components/Detail/Location.tsx
+++ src/components/Detail/Location.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+// components/Detail/Location.tsx
 
-function Location() {
+import React from 'react';
+import KakaoMapWithCenter from './KakaoMapWithCenter';
+
+interface LocationProps {
+  latitude: number;
+  longitude: number;
+  address: string;
+}
+
+const Location: React.FC<LocationProps> = ({ latitude, longitude, address }) => {
+  const center = { lat: latitude, lng: longitude };
+  const markerPosition = { lat: latitude, lng: longitude };
+
   return (
     <div className="w-full lg:w-[1000px] py-8">
-      <h6 className="text-2xl font-bold mb-2">거래장소</h6>
-      <p>안양역</p>
-      <div>map api 불러오기</div>
+      <h6 className="text-2xl font-bold mb-2">거래 희망장소</h6>
+      <p>{address}</p>
+      {/* 논의 필요 */}
+      <div className="rounded-md mt-4 overflow-hidden">
+        <KakaoMapWithCenter center={center} markerPosition={markerPosition} />
+      </div>
     </div>
   );
-}
+};
 
 export default Location;
