79f81f74d708799f4887dbdb8d2d4238b35c4bcd
youwoon <youwoon5520@gmail.com>
2024-07-13 14:23:31 +0900

    add: 글 작성 , 글 수정 반응형으로 수정

--- src/components/common/KakaoMap/KakaoMap.tsx
+++ src/components/common/KakaoMap/KakaoMap.tsx
@@ -118,7 +118,7 @@ const KakaoMap: React.FC<KakaoMapProps> = ({ onMarkerAddressChange }) => {
   return (
     <>
       <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-      <Map center={center} style={{ width: '534px', height: '253px' }} level={3} onCreate={setMap}>
+      <Map center={center} style={{ width: '100%', height: '0', paddingBottom: '39.53%' }} level={3} onCreate={setMap}>
         {map && (
           <MapMarker position={markerPosition} draggable={true} onDragEnd={(target) => handleMarkerDragEnd(target)} />
         )}
