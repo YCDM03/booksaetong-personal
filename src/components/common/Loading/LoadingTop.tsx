7561858b945d90a1ca91ab055a7e9941b6509574
YCDM03 <ycdm03@gmail.com>
2024-07-14 17:42:37 +0900

    Fix: build error

--- src/components/common/Loading/LoadingTop.tsx
+++ src/components/common/Loading/LoadingTop.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Loading: React.FC = () => {
   return (
     <div className="flex justify-center items-start h-screen w-screen">
-      <img src="/images/loading.gif" alt="Loading" className="w-52 h-52" />
+    <Image src="/images/loading.gif" alt="Loading" width={208} height={208} />
     </div>
   );
 };
