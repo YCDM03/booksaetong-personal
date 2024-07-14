0b72d5208d9f57411d57a015173a569403a9d165
백현명 <whitewise95@users.noreply.github.com>
2024-07-14 17:19:22 +0900

    fix: 타입에러 수정

--- src/components/list/CategoryFilter.tsx
+++ src/components/list/CategoryFilter.tsx
@@ -1,12 +1,18 @@
 import { Checkbox, CheckboxProps } from 'antd';
 import React from 'react';
-import useSearchStore, { searchStoreType } from '@/zustand/searchStore';
+import useSearchStore, { SearchStoreType } from '@/zustand/searchStore';
 
-function CategoryFilter({ checkBoxOptions, showFilter, setShowFilter }) {
+type CategoryFilterType = {
+  checkBoxOptions: string[];
+  showFilter: boolean;
+  setShowFilter: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function CategoryFilter({ checkBoxOptions, showFilter, setShowFilter }: CategoryFilterType) {
   const {
     search: { categoryList },
     setCategoryList
-  } = useSearchStore<searchStoreType>((state) => state);
+  } = useSearchStore<SearchStoreType>((state) => state);
 
   const checkAll = checkBoxOptions.length === categoryList.length;
   const indeterminate = categoryList.length > 0 && categoryList.length < checkBoxOptions.length;
@@ -22,19 +28,20 @@ function CategoryFilter({ checkBoxOptions, showFilter, setShowFilter }) {
   return (
     <div className="relative w-[430px] sm:w-[200px] px-[24px] py-[24px] border border-gray-300 rounded-lg">
       <div className="absolute top-2 right-2 sm:hidden">
-        <button
-          className=" text-2xl"
-          onClick={() => setShowFilter(!showFilter)}
-        >
+        <button className=" text-2xl" onClick={() => setShowFilter(!showFilter)}>
           X
         </button>
       </div>
-      <Checkbox className="pb-2" indeterminate={indeterminate}
-                onChange={onCheckAllChange}
-                checked={checkAll}> 전체</Checkbox>
-      <Checkbox.Group className="gap-[10px] flex flex-row sm:flex-col" options={checkBoxOptions}
-                      value={categoryList}
-                      onChange={onChange} />
+      <Checkbox className="pb-2" indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
+        {' '}
+        전체
+      </Checkbox>
+      <Checkbox.Group
+        className="gap-[10px] flex flex-row sm:flex-col"
+        options={checkBoxOptions}
+        value={categoryList}
+        onChange={onChange}
+      />
     </div>
   );
 }
